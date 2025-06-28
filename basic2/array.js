340f5d201e4f71ca77baedc1adaddd3ae92b143b
chandan kumar <chandankumar700451@gamil.com>
2025-06-28 20:20:18 +0530

    Add initial project files including .gitignore, package.json, and package-lock.json; implement array and date manipulation examples in array.js and datetime.js

---
+++ basic2/array.js
@@ -0,0 +1,38 @@
+/*
+let myarr = [1, 2, 3, 4, 5];
+console.log(myarr);
+
+let myarr2 = new Array(1, 2, 3, 4, 5);
+console.log(myarr2);
+
+myarr2[0] = 10;
+console.log(myarr2);
+*/
+
+
+/*
+let myarr2 = new Array(1, 2, 3, 4, 5);
+
+console.log(myarr2.unshift(0)); // Adds 0 to the beginning of the array
+
+console.log(myarr2); // [0, 1, 2, 3, 4, 5]
+const myarr = myarr2.join(", ");
+console.log(myarr); // "0, 1, 2, 3,4, 5"
+
+console.log(myarr2.slice(1,3)); // [1, 2]
+console.log(myarr2.slice(myarr2)); // [1, 2, 3, 4, 5]
+
+console.log(myarr2.splice(1, 2)); // Removes 2 elements starting from index 1
+console.log(myarr2); // [0, 3, 4, 5]
+*/
+ 
+
+const  color = ["red", "blue", "green", "yellow", "purple"];
+const color2 = new Array("red", "blue", "green", "yellow", "purple");
+const result=color.concat(color2); // Concatenates color2 to color
+console.log(color);
+console.log(color2);
+console.log(result); // ["red", "blue", "green", "yellow", "purple", "red", "blue", "green", "yellow", "purple"]
+
+const allcolors =[...color, ...color2]; // Spread operator to combine arrays
+console.log(allcolors);  
---
+++ datetime.js
@@ -0,0 +1,19 @@
+let mydate = new Date();
+console.log(mydate.toString());
+console.log(mydate.toDateString());
+console.log(mydate.toTimeString());
+//console.log(mydate.toISOString());
+console.log(mydate.toLocaleString());
+
+console.log(typeof(mydate));
+
+// console.log(mydate.getFullYear());
+// console.log(mydate.getMonth()); 
+// console.log(mydate.getDate());
+let mydate2 = new Date(2023, 9, 1, 12, 0, 0);
+console.log(mydate2.toString());
+let date = new Date("2023-10-01");
+console.log(date.toLocaleString());
+
+let timeStmap = Date.now();
+console.log(timeStmap);
