82a971c0542586c9d23606d50c69a6650e3e4721
chandan kumar <chandankumar700451@gamil.com>
2025-06-29 21:18:12 +0530

    Refactor scope examples in scopes.js; comment out console logs for clarity

--- basics_function/scopes.js
+++ basics_function/scopes.js
@@ -7,10 +7,36 @@ let c = 60;
     let a = 100; // Block-scoped variable, only accessible within this block
     const b = 200; // Block-scoped constant, only accessible within this block
     var d = 300; // Function-scoped variable, accessible outside the block
-    console.log(`Inside block: a = ${a}, b = ${b}, d = ${d} `); // Outputs: a = 100, b = 200, c = 300
+   // console.log(`Inside block: a = ${a}, b = ${b}, d = ${d} `); // Outputs: a = 100, b = 200, c = 300
 }
 
 
+//console.log(`Outside block: a = ${a}, b = ${b}, c = ${c}`); // Outputs: a = 45, b = 50, c = 300
+
+function testScope() {
+usernaem = "JohnDoe"; // Function-scoped variable
+ passward = "12345"; // Function-scoped variable
+ function test(){
+    const website = "example.com"; // Block-scoped variable
+    console.log(`Username: ${usernaem}, Password: ${passward}`); // Outputs: Username: JohnDoe, Password: 12345
+     
+ }
+   // console.log(`Website: ${website}`); // Outputs: Website: example.com
+    test();
+    
+}
+
+testScope(); // Call the function to see the output
+console.log(add(5)); // Outputs: 6
+function add(num){
+    return num+1
+}
 
 
-console.log(`Outside block: a = ${a}, b = ${b}, c = ${c}`); // Outputs: a = 45, b = 50, c = 300
+//console.log(add2(5)); // Outputs: ReferenceError: Cannot access 'add2' before initialization
+// console.log(add2(5)); // Outputs: 7
+const add2 = function(num){
+    return num+2
+}
+
+console.log(add2(5)); // Outputs: 8
