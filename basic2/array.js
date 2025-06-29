5f2450a16fd053064354c553ecbcba3dc7a8e4ae
chandan kumar <chandankumar700451@gamil.com>
2025-06-29 09:15:57 +0530

    Add object.js with singleton and object literal examples; include symbol usage and methods

--- basic2/array.js
+++ basic2/array.js
@@ -26,7 +26,7 @@ console.log(myarr2.splice(1, 2)); // Removes 2 elements starting from index 1
 console.log(myarr2); // [0, 3, 4, 5]
 */
  
-
+/*
 const  color = ["red", "blue", "green", "yellow", "purple"];
 const color2 = new Array("red", "blue", "green", "yellow", "purple");
 const result=color.concat(color2); // Concatenates color2 to color
@@ -36,3 +36,21 @@ console.log(result); // ["red", "blue", "green", "yellow", "purple", "red", "blu
 
 const allcolors =[...color, ...color2]; // Spread operator to combine arrays
 console.log(allcolors);  
+
+console.log(color.indexOf("blue")); // 1
+
+*/
+
+// Array methods and properties
+/*
+console.log(Array.isArray([1, 2, 3])); // true
+console.log(Array.from("Hello")); // ['H', 'e', 'l', 'l', 'o']
+console.log(Array.isArray("Hello")); // false
+
+console.log(Array.isArray({name:"John"})); // false
+
+*/
+
+
+//array objects
+
